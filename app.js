84498190db85b30b21115b0f4762f09498d1dcd0
enesgituser <enes_gkaya@hotmail.com>
2021-08-07 19:14:39 +0300

    app.js guncellendi.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const server = express();
 
+const port = process.env.PORT || '5000';
+
 server.get("/", (req,res) => {
     res.send("Hello from nodejs express!!");
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log("hello from console.!!!");
 })
